2c6c2d3677b0c290a386a603a840ff20c5578999
colburncodes <colburnsanders@gmail.com>
2021-01-16 10:36:46 -0800

    useState arrays

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleDelete = (id) => {
+    let item = people.filter((person) => person.id !== id);
+    setPeople(item);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <ul key={id} className="item">
+            <li>{name}</li>
+            <button
+              type="button"
+              className="delete-btn"
+              onClick={() => handleDelete(id)}
+            >
+              Delete Item
+            </button>
+          </ul>
+        );
+      })}
+      <button type="button" className="btn" onClick={() => setPeople([])}>
+        Clear Items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
